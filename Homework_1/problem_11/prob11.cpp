61aa8cad8aadd3ea9ca959e8f43add01dfc492dc
kali <kali@kali.kali>
2025-02-11 18:22:57 +0400

    Added

--- Homework_1/problem_11/prob11.cpp
+++ Homework_1/problem_11/prob11.cpp
@@ -14,7 +14,7 @@ int main(){
 	}
 
 	for (int i = 1; i < N; i++) {
-		for (int k = N-1; k >= i; k--) cout << ' ';
+		for (int k = N-1; k > i; k--) cout << ' ';
 		for (int j = 1; j <= i; j++) cout << '*';
 		cout << endl;
 	}
